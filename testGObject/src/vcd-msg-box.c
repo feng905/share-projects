5dfea6956daa9edf8de7683242871f98804d2f8e
root <root@localhost.localdomain>
2019-11-08 16:38:58 +0800

    [testGObject] msg-box add timer

--- testGObject/src/vcd-msg-box.c
+++ testGObject/src/vcd-msg-box.c
@@ -1,8 +1,9 @@
 
 #include <glib.h>
 #include <gtk/gtk.h>
+#include "header.h"
 
-#define VCD_PIC_PATH "/Volumes/feng-sd/code-back/share-projects/testGObject/img/"
+#define VCD_PIC_PATH "/root/feng/share-projects/testGObject/img/"
 
 void set_css(GtkWidget *widget, const gchar *data_css)
 {
@@ -22,6 +23,20 @@ void set_img(GtkWidget *win, const char *widget_class, const char *pic_name)
     set_css(win, css_string);
 }
 
+static gboolean timer_callback(gpointer data){
+    GtkWidget* btn = (GtkWidget*)data;
+
+    static int num = 5;
+    if(num <= 0){
+        g_signal_emit_by_name(btn,"clicked");
+        return FALSE;
+    }
+
+    char text[50] = {0};
+    sprintf(text,"关闭 (%d)",num--);
+    gtk_button_set_label(btn,text);
+    return TRUE;
+}
 
 void message_box(const char *information)
 {
@@ -31,40 +46,40 @@ void message_box(const char *information)
     gtk_window_set_keep_above(GTK_WINDOW(window_message_box), TRUE);
     gtk_window_set_position(GTK_WINDOW(window_message_box), GTK_WIN_POS_CENTER_ALWAYS);//居中
     gtk_container_set_border_width(GTK_CONTAINER(window_message_box), 50);
-    gtk_window_set_decorated(GTK_WINDOW(window_message_box), TRUE);//无边框
+    gtk_window_set_decorated(GTK_WINDOW(window_message_box), FALSE);//无边框
 
-//    fix = gtk_fixed_new();
+    fix = gtk_fixed_new();
 
-//    char tmp_info[120];
-//    snprintf(tmp_info,sizeof(tmp_info),"%s", information);
-//    label = gtk_label_new(tmp_info);
+    char tmp_info[120];
+    snprintf(tmp_info,sizeof(tmp_info),"%s", information);
+    label = gtk_label_new(tmp_info);
 
-//    btn_ok = gtk_button_new_with_label("关闭 1");
-//    gtk_button_set_relief(GTK_BUTTON(btn_ok), GTK_RELIEF_NONE);
-//    gtk_widget_set_size_request(btn_ok, 150, 0);
+    btn_ok = gtk_button_new_with_label("关闭");
+    gtk_button_set_relief(GTK_BUTTON(btn_ok), GTK_RELIEF_NONE);
+    gtk_widget_set_size_request(btn_ok, 150, 0);
 
-//    label_pic = gtk_label_new("");
-//    gtk_widget_set_size_request(label_pic, 70, 70);
+    label_pic = gtk_label_new("");
+    gtk_widget_set_size_request(label_pic, 70, 70);
 
-//    scrolled_win  = gtk_scrolled_window_new(NULL, NULL);
-//    gtk_widget_set_size_request(scrolled_win, 250, 0);
-//    gtk_container_add(GTK_CONTAINER(scrolled_win), label);
+    scrolled_win  = gtk_scrolled_window_new(NULL, NULL);
+    gtk_widget_set_size_request(scrolled_win, 250, 0);
+    gtk_container_add(GTK_CONTAINER(scrolled_win), label);
 
-//    gtk_fixed_put((GtkFixed *)fix, label_pic, 85, 20);
-//    gtk_fixed_put((GtkFixed *)fix, scrolled_win, 0, 100);
-//    gtk_fixed_put((GtkFixed *)fix, btn_ok, 50, 155);
+    gtk_fixed_put((GtkFixed *)fix, label_pic, 85, 20);
+    gtk_fixed_put((GtkFixed *)fix, scrolled_win, 0, 100);
+    gtk_fixed_put((GtkFixed *)fix, btn_ok, 50, 155);
 
-//    gtk_container_add(GTK_CONTAINER(window_message_box), fix);
+    gtk_container_add(GTK_CONTAINER(window_message_box), fix);
 
-//    g_signal_connect_swapped(G_OBJECT(btn_ok), "clicked", G_CALLBACK(gtk_widget_destroy), window_message_box);
+    g_signal_connect_swapped(G_OBJECT(btn_ok), "clicked", G_CALLBACK(gtk_widget_destroy), window_message_box);
 
-
-     //set_img(label_pic, "label", "gantanhao.png");
-//     set_css(window_message_box, "window{border-radius:0px;background-color: rgb(230, 230, 230)}");
-     set_css(window_message_box, "window{border-radius:2px;background-color: rgb(256, 256, 0)}");
-    // set_css(btn_ok, "button{color: white;background: rgb(53, 126, 169);border: 1px solid rgb(40, 94, 142);border-radius: 0px}");
-     //set_css(btn_ok, "button:hover{border-radius:0px;background: #3070a9}");
+    set_img(label_pic, "label", "gantanhao.png");
+    set_css(window_message_box, "window{border-radius:0px;background-color: rgb(230, 230, 230)}");
+    set_css(btn_ok, "button{color: white;background: rgb(53, 126, 169);border: 1px solid rgb(40, 94, 142);border-radius: 0px}");
+    set_css(btn_ok, "button:hover{border-radius:0px;background: #3070a9}");
 
     gtk_widget_show_all(window_message_box);
+
+    g_timeout_add_seconds(1, timer_callback, btn_ok);
 }
 
